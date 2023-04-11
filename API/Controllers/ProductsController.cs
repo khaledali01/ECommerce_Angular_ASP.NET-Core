0879d2eefab4a4f98ed4fa7134a1d4b284516892
Khaled <khaled.ali.architect@gmail>
2023-04-11 20:35:37 +0200

    Creating API Project
    See: #1

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "single product";
+        }
+
+
+    }
+}
